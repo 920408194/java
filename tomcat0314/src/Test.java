56801737d92892937a6847f47e2663a7d81f887f
panda <13217322877@163.com>
2020-03-18 10:10:58 +0800

    1

--- tomcat0314/src/Test.java
+++ tomcat0314/src/Test.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
 /**
@@ -6,11 +7,14 @@ import java.util.logging.Logger;
  * @date 2020/3/15
  */
 public class Test {
-    public static void main(String[] args) {
-        Logger logger = Logger.getGlobal();
-        logger.info("1");
-        logger.warning("memory is running out...");
-        logger.fine("ignored.");
-        logger.severe("process will be terminated...");
+    public static void main(String[] args) throws Exception {
+        // String对象:
+        String s = "Hello world";
+        // 获取String substring(int)方法，参数为int:
+        Method m = String.class.getMethod("substring", int.class);
+        // 在s对象上调用该方法并获取结果:
+        String r = (String) m.invoke(s, 6);
+        // 打印调用结果:
+        System.out.println(r);
     }
 }
--- tomcat0314/src/com/panda/tomcat0314/Test.java
+++ tomcat0314/src/com/panda/tomcat0314/Test.java
@@ -12,8 +12,9 @@ public class Test {
     public static void main(String[] args) {
         //try catch处理异常
         try {
-            //反射加载驱动
-            Class.forName("com.mysql.cj.jdbc.Driver");
+//            //反射加载驱动，jdbc4.0好像不用这步操作
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+
             //获取连接地址,并处理中文乱码
             String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8";
             //用户信息
@@ -23,8 +24,12 @@ public class Test {
             Connection connection = DriverManager.getConnection(url,usr,password);
             //获取statement并执行语句
             Statement statement = connection.createStatement();
-            //String sql = "insert into foods(title,owner,price,date,sold,image) values('可乐','panda',2.5,'2020-01-01',0,'n')";
-            //statement.executeUpdate(sql);
+
+//            //insert,update,delete等语句需要使用executeUpdate
+//            String sql = "insert into foods(title,owner,price,date,sold,image) values('可乐','panda',2.5,'2020-01-01',0,'n')";
+//            statement.executeUpdate(sql);
+
+            //查询需要使用executeQuery
             String sql = "select * from foods";
             ResultSet resultSet =  statement.executeQuery(sql);
             while (resultSet.next()){
@@ -38,7 +43,7 @@ public class Test {
                 System.out.println(title);
 
             }
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (/*ClassNotFoundException |*/ SQLException e) {
             e.printStackTrace();
         }
     }
