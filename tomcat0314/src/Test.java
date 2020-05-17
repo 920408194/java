ebe5d38323e18359c2e2ccca1d3cdf64528b2930
panda <13217322877@163.com>
2020-05-18 00:45:33 +0800

    1

--- tomcat0314/src/Test.java
+++ tomcat0314/src/Test.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
 /**
  * @author panda
  * @description
  * @date 2020/3/15
  */
 public class Test {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        input.close();
 
+        Node[] puke = new Node[length];
+        //init the Node
+        for(int i=0; i<length; i++) {
+            Node n = new Node();
+            n.next = i+1;
+            puke[i] = n;
+        }
+        puke[length-1].next = 0;
+        //do
+        for(int i=0, k=1;k<=length ;k++){
+            int index = puke[i].next;
+            puke[index].value = k;
+            puke[i].next = puke[index].next;
+            i = puke[i].next;
+        }
+       for (int i=0; i<length; i++)
+           System.out.print(puke[i].value+" ");
+        System.out.println("");
     }
 
-    public @interface Report {
-        int type() default 0;
-        String level() default "info";
-        String value() default "";
+    private static class Node{
+        int value;
+        int next;
     }
 }
